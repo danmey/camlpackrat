ec76c291a34fa7ac1145a521ca2683924a39caab
Wojciech Meyer <specuu@gmail.com>
2009-02-27 18:25:26 +0000

    before changing nested arrays to matrices

--- src/peg.ml
+++ src/peg.ml
@@ -1,77 +1,92 @@
-module Peg = struct
-  type 'a memo_result = {poss:int; pose:int; result:'a}
-  type 'a memo_slot = NotYet | Remembered of 'a memo_result | Failed
-  type 'a stream = ('a  array array * int)
-  exception Noparse 
+(* type 'a memo_type = Remembered of 'a * int * int | NotYet | Failed *)
+(* type 'a memo = { string:string; memo:'a memo_type array array } *)
+(* type 'a stream = { current:int; memo_table:'a memo } *)
+(* exception Noparse *)
 
-    
-  let make_stream str = 
-    let s = Array.make 10 (Array.make (String.length str) NotYet) in
-    let rec loop i =
-      if i < 0 then s,0 else begin
-	let chr = (String.get str i) in
-	  s.(0).(i) <- (Remembered {poss=i;pose=i+1;result=chr}); 
-	  loop (i-1)  
-      end
-    in loop ((String.length str)-1) 
-  
-  let get_memo stream combinator rule_id = 
-    let ar, position = stream in
-    match ar.(rule_id).(position) with
-      | Remembered a -> a
-      | NotYet -> let result = combinator stream in 
-	  Array.set ar.(position) rule_id (Remembered(result)); result
-      | Failed -> raise Noparse ;;
+(* let init_memo str = {string=str; memo=Array.create 1 (Array.create (String.length str) NotYet)} *)
 
-  let end_of_stream = function str,pos -> Array.length str.(0) <= pos 
+(* let get_result memo current rule_id f = *)
+(*   let num_rules = Array.length memo.memo in *)
+(*   let a_memo = if rule_id >= num_rules then *)
+(*     let num_difference = rule_id - (num_rules-1) in *)
+(*     let new_arrays = Array.create num_difference (Array.create (String.length memo.string) NotYet) in *)
+(*     let new_memo_arrays = Array.append memo.memo new_arrays in *)
+(*       {memo with memo=new_memo_arrays} *)
+(*   else memo in *)
+(*   let result =  *)
+(*     let a_rule = Array.get a_memo.memo rule_id in *)
+(*       match Array.get a_rule current with *)
+(* 	| NotYet -> f rule_id {current=current; memo_table=a_memo}  *)
+(* 	| (Remembered(a,startp,endp)) -> a, {current=endp; memo_table=a_memo} *)
+(* 	| _ -> raise Noparse *)
+(*   in *)
+(*     result *)
+      
 
-  let get_result str rule_id = 
-    let s,pos = str in
-      if not (end_of_stream str) then match s.(rule_id).(pos) with
-	| Remembered r -> r
-	| _ -> Printf.printf "bala\n";raise Noparse 
-      else raise Noparse 
+(* let pmatch  ch rule_id str =  *)
+(*   let the_char = String.get str.memo_table.string str.current in *)
+(*     if the_char == ch then *)
+(*       [ch], {str with current=str.current+1;} *)
+(*     else raise Noparse *)
 
-  let advance = function str, pos -> (str, pos+1)
-  let k f b = if f b != b then raise Noparse else b
+(* let choicei p1 p2 rule_id str = *)
+(*   try p1 rule_id str with *)
+(*     Noparse -> p2 rule_id str *)
 
-  let some f str _ =  let r = (get_result str 0) in if f r.result then r, (advance str) else raise Noparse
+(* let choice p1 p2 rule_id str = get_result str.memo_table str.current rule_id (choicei p1 p2) *)
 
-  let rec many p s id =
-      let rec loop str =
-	try
-	  let result, next = p str id in
-	  let results,rest = (loop next) in
-	    result.result::results,rest
-	with Noparse -> [],str in
-      let r = loop s in
-	{poss=snd s;pose=snd (snd r);result=fst r},s
-	  
+type 'a memo_type = Remembered of 'a * int * int | NotYet | Failed
+type 'a memo = { string:string; memo:'a memo_type array array }
+type 'a stream = { current:int; memo_table:'a memo }
+exception Noparse
 
-  let new_rules()  = 
-    let n = ref 0 in 
-      (fun () -> n := !n+1; n);;
-  
+let init_memo str = {string=str; memo=Array.create 1 (Array.create (String.length str) NotYet)}
+let make_stream str = {current=0; memo_table=init_memo str}
+let get_result memo current rule_id f =
+   
+let num_rules = Array.length memo.memo in
+  try
+    let a_memo = if rule_id >= num_rules then
+      let num_difference = rule_id - (num_rules-1) in
+      let new_arrays = Array.create num_difference (Array.create (String.length memo.string) NotYet) in
+      let new_memo_arrays = Array.append memo.memo new_arrays in
+	{memo with memo=new_memo_arrays}
+    else memo in
+    let result = 
+      let a_rule = Array.get a_memo.memo rule_id in
+	match Array.get a_rule current with
+	  | NotYet ->   let r = f rule_id {current=current; memo_table=a_memo} in
+	    let an_array = (Array.get a_memo.memo rule_id) in
+	      Array.set an_array current (Remembered(fst r, current, (fst r).current));
+	      Array.set a_memo.memo rule_id an_array;
+	      r
+	  | (Remembered(a,startp,endp)) -> a, {current=endp; memo_table=a_memo}
+	  | _ -> raise Noparse
+  in
+      result
+ with Invalid_argument a -> raise Noparse
+      
 
+let pmatch ch rule_id str = 
+  try 
+    let the_char = String.get str.memo_table.string str.current in
+    if the_char == ch then
+      [ch], {str with current=str.current+1;}
+    else raise Noparse
+  with Invalid_argument a -> raise Noparse
 
-  let bind p f s id =
-    let result,rest = p s id in
-      {result with result=f result.result},rest
+let choicei p1 p2 rule_id str =
+  try p1 rule_id str with
+    Noparse -> p2 rule_id str
 
-	
-  type ast = One | Zeros of int
-  ;;
+let choice p1 p2 rule_id str = get_result str.memo_table str.current rule_id (choicei p1 p2)
 
-  let parse_rule a_rule transform str id = 
-    bind a_rule transform str id
-      ;;
-  
-(*  
-  let parser string =
-    let str = make_stream string in
-    let nr = new_rules() in
-    let first = parse_rule (many (some (fun x -> x == '0'))) (fun l -> Zeros (List.length l)) str nr in
-      fst first
-*)
-let _ = 'a'     
-end
+let rec manyi p rule_id s =
+  try
+    let result, next = p rule_id s in
+    let results,rest = (manyi p rule_id next ) in
+      result@results,rest
+  with Noparse -> [],s
+
+let many p rule_id str = get_result str.memo_table str.current rule_id (manyi p)
+
