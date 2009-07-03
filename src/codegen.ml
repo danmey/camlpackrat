cb95125f85f68dc8289bab2995ac99ac440dc986
Wojciech Meyer <specuu@gmail.com>
2009-07-03 09:27:49 +0100

    caching

--- src/codegen.ml
+++ src/codegen.ml
@@ -15,6 +15,7 @@ type parser_lang =
   | Check of char * parser_lang * parser_lang 
   | CheckRange of string * char * char * parser_lang * parser_lang
   | CheckCache of string * parser_lang
+  | Cache of string * parser_lang
   | CustomCode of string 
   | Block of parser_lang list 
   | TopLevel of string * parser_lang 
@@ -29,14 +30,14 @@ type parser_lang =
   | AppendResult of string
   | ResetVars of parser_lang
 
-let std_var = "___nm"
+let std_var = "loc"
  
 (* The rule succeeded drop position stack, and return success *)
-let rule_success var =
-  Block [Drop; RetSuccess var]
+let rule_success name var =
+  Block [Drop; Cache(name, RetSuccess var)]
 
 (* Rule failed pop out position stack and return failure *)
-let rule_fail = Block [Pop; RetFail]
+let rule_fail name = Block [Pop; Cache(name, RetFail)]
 
 let mk_generator () = 
   let x = ref 0 in
@@ -70,7 +71,9 @@ let match_string str succ fail =
 				    succ))
   
 (* Construct top level rule *)    
-let toplevel_rule rule_id body = TopLevel (String.lowercase rule_id,Block[Push; body])
+let toplevel_rule rule_id body = 
+  let id = String.lowercase rule_id in
+    TopLevel (id,CheckCache(id, Block[Push; body]))
 
 let rec drop_if f = function
     [] -> []
@@ -82,24 +85,8 @@ let take_if f lst =
   | h::t -> if f h then loop (acc@[h]) t else acc in
     loop [] lst
 
-(* Replace place holders with free variables bound to chunks processed *)
-(*
-let rec replace_placholders s = 
-  let str = explode s in
-  let before = take_if (fun x -> x != '$') str in
-  let after = drop_if (fun x -> x != '$') str in
-    if after != [] then
-      let a_number = take_if (fun x -> x = '$' || x >= '0' && x < '9') after in
-      let after_number = drop_if (fun x -> x = '$' || x >= '0' && x < '9') after in
-	match a_number with
-	    [] -> implode str
-	  | h::t -> replace_placholders (implode (before @ (explode "r_res_") @ t @ after_number))
-    else 
-      s
-*)
-
 (* Transform ast to parser language *)
-let rule_body ast = 
+let rule_body name ast = 
   let rec char_class var succ fail = function
     | Range (ch1, ch2) -> Assign (var, Fetch, (CheckRange (var, ch1,ch2, succ, fail)))
     | Classes cls ->    List.fold_right (fun el acc -> 
@@ -121,25 +108,23 @@ let rule_body ast =
     | Nothing -> Block[]
     | AssignVar (n, bl) -> rule_body' n succ fail bl
   in
-(*
-  let resolve_variables ast = 
-    let rec resolve_variables' n  = function 
-      | Assign(_, Fetch, CheckRange(_, ch1,ch2, s, f ) ) -> Assign(n, Fetch, CheckRange(var, ch1,ch2, resolve_variables' (n+1) s, resolve_variables' (n+1) f ) ) 
-      | Assign(_, v, b) -> Assign(n, resolve_variables' (n+1) v, resolve_variables' (n+1) b)
-      | MatchRule(str,_,s,f) -> MatchRule(str, n, resolve_variables' (n+1) s, resolve_variables' (n+1) f)       
-      | Loop (_,s,f) -> Loop(n, resolve_variables' (n+1) s, resolve_variables' (n+1) f)       
-      | EscapeLoop _ -> EscapeLoop (n-1)
-      | RetSuccess _ -> RetSuccess (n-1)
-      | AppendResult _ -> AppendResult (n-1)
-      | Check (c, a, b) -> Check (c, resolve_variables' n a,resolve_variables' n b)
-      | Block l -> Block (List.map (resolve_variables' n) l)
-      | ResetVars l -> resolve_variables' 0 l
-      | els -> els
-
-    in resolve_variables' 0 ast in
-    resolve_variables (rule_body' var rule_success rule_fail ast) 
-*)
-    rule_body' std_var (rule_success std_var) rule_fail ast
+  let rec reduce_pass = function
+    | Assign(var1, CustomCode(var2), body) -> 
+	if var1 = var2 then 
+	  reduce_pass body 
+	else
+	  Assign(var1, CustomCode(var2), reduce_pass body)
+    | Check (ch, b1, b2) -> Check (ch, reduce_pass b1, reduce_pass b2) 
+    | CheckRange(str,ch1,ch2,b1,b2) -> CheckRange(str,ch1,ch2,reduce_pass b1,reduce_pass b2)
+    | Block lst ->  Block (List.map reduce_pass lst)
+    | TopLevel (str, b) -> TopLevel (str, reduce_pass b) 
+    | MatchRule (str1, str2, b1, b2) -> MatchRule (str1, str2, reduce_pass b1, reduce_pass b2) 
+    | Loop (str, b1, b2) -> Loop (str, reduce_pass b1, reduce_pass b2)
+    | Assign (str,b1, b2) -> Assign (str,reduce_pass b1, reduce_pass b2)
+    | ResetVars b -> ResetVars b 
+    | a -> a 
+  in
+    (rule_body' std_var (rule_success name std_var) (rule_fail name) ast)
   
 
 let declarations rules = 
@@ -152,18 +137,18 @@ let rec string_of_parser_lang plang =
   let rec ident str i = if i > 0 then ident ("  " ^ str) (i-1) else str in
   let rec loop t = 
     function
-      | Push -> [t, "let _ = Mstream.push stream in"]
-      | Pop -> [t, "let _ = Mstream.pop stream in"]
-      | Drop -> [t, "let _ = Mstream.drop stream in"]
+      | Push -> [t, "Mstream.push stream;"]
+      | Pop -> [t, "Mstream.pop stream;"]
+      | Drop -> [t, "Mstream.drop stream;"]
       | RetSuccess n -> [t,"Mlpdef.Success ((Mstream.spos stream), " ^ n ^ ")"]
       | RetFail -> [t, "Mlpdef.Fail"]
       | Assign (n,v, b) -> [t, "let " ^ n ^ " = "]@loop t v@[t," in";t,"begin"]@loop t b@[t,"end"]
       | Const s -> [t, s]
-      | MatchRule (r,n,s,f) -> [(t,"match " ^ r ^ " stream with")]@[((t+1),"| Mlpdef.Fail -> ")]@loop t f@[(t+1, "| Mlpdef.Success(_, " ^ n ^ ") -> let " ^ std_var ^ " = " ^ n ^ " in ")]@loop t s
+      | MatchRule (r,n,s,f) -> [(t,"match " ^ r ^ " stream with")]@[((t+1),"| Mlpdef.Fail -> ")]@loop t f@[(t+1, "| Mlpdef.Success(_, " ^ n ^ ") -> " ^ if not (std_var = n) then "let " ^ std_var ^ " = " ^ n ^ " in " else "")]@loop t s
       | Block lst -> [t,"begin"]@(List.map (function (t2,x) -> (t2+1), x) (List.concat (List.map (loop t) lst)))@[t,"end"]
       | Check (ch, s, f) -> [t, "if (Mstream.next stream).r_base = " ^ squote (string_of_char ch) ^ " then"]@loop t s@[t,"else"]@loop t f
       | TopLevel (n, b) -> [t, "and " ^ n ^ " stream = "]@(loop (t+1) b)
-      | Struct lst -> [(t,"type memo = {r_base:char;")]@(List.fold_left (fun acc -> function id,typ -> acc@[t+1, id ^ " :" ^ typ ^ " option;"]) [] lst)@[t,"}"]
+      | Struct lst -> [(t,"type memo = {r_base:char;")]@(List.fold_left (fun acc -> function id,typ -> acc@[t+1, "mutable " ^ id ^ " :" ^ typ ^ " Mlpdef.result option;"]) [] lst)@[t,"}"]
       | InitStruct lst -> [(t,"let rec memo_table_init ch = {")]@[t+1, "r_base=ch;"]@
 	  (List.fold_left (fun acc -> function id,typ -> acc@[t+1, id ^ "= None;"]) [] lst)@[t,"}"] 
       | Entry n -> [t, "let parse str = (Mlpdef.parse memo_table_init str " ^ n ^")"]
@@ -179,13 +164,23 @@ let rec string_of_parser_lang plang =
 	  [t, "if " ^ n ^ ".[0]" ^ " >= " ^ squote (string_of_char ch1) ^ " && " ^ n ^ ".[0]" ^ " <= " ^ squote (string_of_char ch2) ^ " then"]
 	    @ loop (t+1) s @ [t+1," else "] @ loop (t+1) f
       | ResetVars ls -> loop t ls
+      | CheckCache (str, body) -> 
+	    [t, "let " ^ std_var ^ " = (Mstream.top stream)." ^ str ^ " in"]
+	  @ [t, "match " ^ std_var ^ " with"]
+	  @ [t, "| Some a -> a"]
+	  @ [t, "| None ->"]
+	  @ loop t body
+      | Cache (str, body) -> 
+	    [t,"(Mstream.top stream). " ^ str ^ "<- Some ( "]
+	  @ loop t body @ [t,");"] @ loop t body
+	  
   in
   let lst = loop 0 plang in
     String.concat "\n"  (List.fold_left (fun acc -> function t,str -> acc @ [ident str t]) [] lst)
 
 
 let one_rule r = 
-  toplevel_rule r.rule_id (rule_body r.rule_body)
+  toplevel_rule r.rule_id (rule_body (String.lowercase r.rule_id) r.rule_body)
 
 let gen_code str = 
     let lexbuf = Lexing.from_string str in
